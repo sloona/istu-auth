308e1daa4c01f41f20ef44dac5a6a90c1fccde1d
sloona <sloona@mail.ru>
2017-05-05 13:14:25 +0400

    add web service

--- Istu.Elma.Web.AutoAuth/Istu.Elma/Istu.Elma.Auth/Repository/AuthRepository.cs
+++ Istu.Elma.Web.AutoAuth/Istu.Elma/Istu.Elma.Auth/Repository/AuthRepository.cs
@@ -4,19 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Istu.Elma.Auth.Models;
+using Istu.Elma.Auth.ElmaService;
+
 
 namespace Istu.Elma.Auth.Repository
 {
     class AuthRepository
     {
-        public Istu.Elma.Auth.Models.Auth Create()
+        public Models.Auth Create()
         {
-            return new Istu.Elma.Auth.Models.Auth();
+            return new Models.Auth();
         }
 
         public void CreateElmaTask()
         {
-
+            var elmaService = new WFPWebServiceSoapClient();
+            var data = new WebData();
+            var t = new WebDataItem();
+            t.Name = "test"; t.Value = "test value";
+            data.Items = new WebDataItem[] { new WebDataItem{Name = "testName", Value = "testValue" }, new WebDataItem { Name = "testName", Value = "testValue" }};
+           elmaService.Run("userName", "password", "token", "instanceName", data);
         }
     }
 }
